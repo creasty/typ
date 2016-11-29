ffef2a82915dcb73514d0ea2d11cba87dcaf36fb
Yuki Iwanaga <yuki@creasty.com>
2016-11-29 17:36:16 +0900

    Add more methods

--- int/set.go
+++ int/set.go
@@ -4,6 +4,8 @@ import (
 	"sync"
 )
 
+/*  int
+-----------------------------------------------*/
 type IntSet struct {
 	set  map[int]bool
 	lock sync.Mutex
@@ -17,17 +19,23 @@ func NewIntSet() *IntSet {
 }
 
 func (self *IntSet) Add(item int) {
-	self.lock.Lock()
-	defer self.lock.Unlock()
-
 	self.set[item] = true
 }
 
-func (self *IntSet) Remove(item int) {
+func (self *IntSet) AddSafe(item int) {
 	self.lock.Lock()
 	defer self.lock.Unlock()
+	self.Add(item)
+}
 
+func (self *IntSet) Remove(item int) {
 	delete(self.set, item)
+}
+
+func (self *IntSet) RemoveSafe(item int) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
 }
 
 func (self *IntSet) ToArray() []int {
@@ -35,6 +43,447 @@ func (self *IntSet) ToArray() []int {
 	defer self.lock.Unlock()
 
 	a := make([]int, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  int8
+-----------------------------------------------*/
+type Int8Set struct {
+	set  map[int8]bool
+	lock sync.Mutex
+}
+
+func NewInt8Set() *Int8Set {
+	return &Int8Set{
+		set:  make(map[int8]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Int8Set) Add(item int8) {
+	self.set[item] = true
+}
+
+func (self *Int8Set) AddSafe(item int8) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Int8Set) Remove(item int8) {
+	delete(self.set, item)
+}
+
+func (self *Int8Set) RemoveSafe(item int8) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Int8Set) ToArray() []int8 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]int8, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  int16
+-----------------------------------------------*/
+type Int16Set struct {
+	set  map[int16]bool
+	lock sync.Mutex
+}
+
+func NewInt16Set() *Int16Set {
+	return &Int16Set{
+		set:  make(map[int16]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Int16Set) Add(item int16) {
+	self.set[item] = true
+}
+
+func (self *Int16Set) AddSafe(item int16) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Int16Set) Remove(item int16) {
+	delete(self.set, item)
+}
+
+func (self *Int16Set) RemoveSafe(item int16) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Int16Set) ToArray() []int16 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]int16, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  int32
+-----------------------------------------------*/
+type Int32Set struct {
+	set  map[int32]bool
+	lock sync.Mutex
+}
+
+func NewInt32Set() *Int32Set {
+	return &Int32Set{
+		set:  make(map[int32]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Int32Set) Add(item int32) {
+	self.set[item] = true
+}
+
+func (self *Int32Set) AddSafe(item int32) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Int32Set) Remove(item int32) {
+	delete(self.set, item)
+}
+
+func (self *Int32Set) RemoveSafe(item int32) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Int32Set) ToArray() []int32 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]int32, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  int64
+-----------------------------------------------*/
+type Int64Set struct {
+	set  map[int64]bool
+	lock sync.Mutex
+}
+
+func NewInt64Set() *Int64Set {
+	return &Int64Set{
+		set:  make(map[int64]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Int64Set) Add(item int64) {
+	self.set[item] = true
+}
+
+func (self *Int64Set) AddSafe(item int64) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Int64Set) Remove(item int64) {
+	delete(self.set, item)
+}
+
+func (self *Int64Set) RemoveSafe(item int64) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Int64Set) ToArray() []int64 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]int64, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  uint
+-----------------------------------------------*/
+type UintSet struct {
+	set  map[uint]bool
+	lock sync.Mutex
+}
+
+func NewUintSet() *UintSet {
+	return &UintSet{
+		set:  make(map[uint]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *UintSet) Add(item uint) {
+	self.set[item] = true
+}
+
+func (self *UintSet) AddSafe(item uint) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *UintSet) Remove(item uint) {
+	delete(self.set, item)
+}
+
+func (self *UintSet) RemoveSafe(item uint) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *UintSet) ToArray() []uint {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]uint, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  uint8
+-----------------------------------------------*/
+type Uint8Set struct {
+	set  map[uint8]bool
+	lock sync.Mutex
+}
+
+func NewUint8Set() *Uint8Set {
+	return &Uint8Set{
+		set:  make(map[uint8]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Uint8Set) Add(item uint8) {
+	self.set[item] = true
+}
+
+func (self *Uint8Set) AddSafe(item uint8) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Uint8Set) Remove(item uint8) {
+	delete(self.set, item)
+}
+
+func (self *Uint8Set) RemoveSafe(item uint8) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Uint8Set) ToArray() []uint8 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]uint8, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  uint16
+-----------------------------------------------*/
+type Uint16Set struct {
+	set  map[uint16]bool
+	lock sync.Mutex
+}
+
+func NewUint16Set() *Uint16Set {
+	return &Uint16Set{
+		set:  make(map[uint16]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Uint16Set) Add(item uint16) {
+	self.set[item] = true
+}
+
+func (self *Uint16Set) AddSafe(item uint16) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Uint16Set) Remove(item uint16) {
+	delete(self.set, item)
+}
+
+func (self *Uint16Set) RemoveSafe(item uint16) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Uint16Set) ToArray() []uint16 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]uint16, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  uint32
+-----------------------------------------------*/
+type Uint32Set struct {
+	set  map[uint32]bool
+	lock sync.Mutex
+}
+
+func NewUint32Set() *Uint32Set {
+	return &Uint32Set{
+		set:  make(map[uint32]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Uint32Set) Add(item uint32) {
+	self.set[item] = true
+}
+
+func (self *Uint32Set) AddSafe(item uint32) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Uint32Set) Remove(item uint32) {
+	delete(self.set, item)
+}
+
+func (self *Uint32Set) RemoveSafe(item uint32) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Uint32Set) ToArray() []uint32 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]uint32, len(self.set))
+
+	i := 0
+	for item := range self.set {
+		a[i] = item
+		i++
+	}
+
+	return a
+}
+
+/*  uint64
+-----------------------------------------------*/
+type Uint64Set struct {
+	set  map[uint64]bool
+	lock sync.Mutex
+}
+
+func NewUint64Set() *Uint64Set {
+	return &Uint64Set{
+		set:  make(map[uint64]bool),
+		lock: sync.Mutex{},
+	}
+}
+
+func (self *Uint64Set) Add(item uint64) {
+	self.set[item] = true
+}
+
+func (self *Uint64Set) AddSafe(item uint64) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Add(item)
+}
+
+func (self *Uint64Set) Remove(item uint64) {
+	delete(self.set, item)
+}
+
+func (self *Uint64Set) RemoveSafe(item uint64) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.Remove(item)
+}
+
+func (self *Uint64Set) ToArray() []uint64 {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	a := make([]uint64, len(self.set))
 
 	i := 0
 	for item := range self.set {
--- int/set_test.go
+++ int/set_test.go
@@ -24,3 +24,192 @@ func Test_IntSet(t *testing.T) {
 		t.Error("expect %v to equal to %v", actual, expect)
 	}
 }
+
+func Test_Int8Set(t *testing.T) {
+	s := NewInt8Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []int8{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_Int16Set(t *testing.T) {
+	s := NewInt16Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []int16{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_Int32Set(t *testing.T) {
+	s := NewInt32Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []int32{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_Int64Set(t *testing.T) {
+	s := NewInt64Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []int64{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_UintSet(t *testing.T) {
+	s := NewUintSet()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []uint{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_Uint8Set(t *testing.T) {
+	s := NewUint8Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []uint8{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_Uint16Set(t *testing.T) {
+	s := NewUint16Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []uint16{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_Uint32Set(t *testing.T) {
+	s := NewUint32Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []uint32{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
+
+func Test_Uint64Set(t *testing.T) {
+	s := NewUint64Set()
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(2)
+	s.Add(3)
+	s.Add(3)
+	s.Add(3)
+
+	s.Remove(3)
+	s.Remove(9)
+
+	actual := s.ToArray()
+	expect := []uint64{1, 2}
+
+	if len(actual) != len(expect) {
+		t.Error("expect %v to equal to %v", actual, expect)
+	}
+}
